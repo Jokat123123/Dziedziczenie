91e603b65e2966497af1944df56609c7d0d1a927
Jokat123123 <mateusz.przybylo2007@gmail.com>
2025-03-06 14:34:17 +0100

    pelen projekt 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        VehicleRegistry registry = new VehicleRegistry();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Owner owner1 = new Owner("Jan Kowalski");
+        Owner owner2 = new Owner("Anna Nowak");
+
+        Vehicle car = new LandVehicle("Toyota Corolla", 2020, owner1, 4);
+        Vehicle boat = new WaterVehicle("Yacht Sunseeker", 2018, owner2, 25.5);
+        Vehicle plane = new AirVehicle("Cessna 172", 2015, owner1, 4000);
+
+        registry.registerVehicle(car);
+        registry.registerVehicle(boat);
+        registry.registerVehicle(plane);
+
+        registry.displayAllVehicles();
     }
 }
